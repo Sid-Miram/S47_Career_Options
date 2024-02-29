9b3076ae92bc926545cec4bd29aa2b7a925c3222
Sid-Miram <beingsilent7777@gmail.com>
2024-02-29 15:21:11 +0530

    Debugged Application

--- route.js
+++ route.js
@@ -1,24 +1,43 @@
+
 const express = require('express');
-const app = express();
+const router = express.Router(); // Create a router instance
 const data = require('./Data.json');
 
-app.use(express.json());
-
-
-const getting = app.get('/getting', (req, res) => {
-    res.send(data);
+// GET route to fetch data
+router.get('/getting', (req, res) => {
+    res.json(data);
 });
 
-const posting = app.post('/posting', (req, res) => {
-    const newData = req.body;
-    // console.log(req.body);
-    data.push(newData);
-    res.status(201).json(newData);
+// POST route to add data
+router.post('/', (req, res) => {
+    const newDay = req.body;
+    const id = `id_${Object.keys(data).length + 1}`;
+    data[id] = newDay;
+    res.json({ id: id, message: "New day added successfully", data: newDay });
 });
 
+// PUT route to update data
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+    if (data[id]) {
+        data[id] = update;
+        res.json({ message: `Day with id ${id} updated successfully`, data: data[id] });
+    } else {
+        res.status(404).json({ error: `Day with id ${id} not found` });
+    }
+});
 
-   
-    // const putting = 
+// DELETE route to delete data
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    if (data[id]) {
+        delete data[id];
+        res.json({ message: `Day with id ${id} deleted successfully` });
+    } else {
+        res.status(404).json({ error: `Day with id ${id} not found` });
+    }
+});
 
+module.exports = router; // Export the router instance
 
-module.exports = { posting, getting};
